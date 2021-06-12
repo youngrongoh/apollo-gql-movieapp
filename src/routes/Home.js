5ecb86fb8a20670fb7edd5b74d81bf21fe07478a
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-12 14:30:55 +0900

    feat: gql query 요청

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,22 @@
 import React from 'react';
-export default () => <h1>Home</h1>;
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return 'loading';
+  }
+  if (data && data.movies) {
+    return data.movies.map((movie) => <h1>{movie.id}</h1>);
+  }
+};
